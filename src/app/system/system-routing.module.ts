6fd75093dbb038f5c674da326c13667e25449f45
Marina Ismailova <marina_ismailova@epam.com>
2017-11-19 03:15:19 +0300

    add theme detail page

--- src/app/system/system-routing.module.ts
+++ src/app/system/system-routing.module.ts
@@ -5,6 +5,7 @@ import { SystemComponent } from './system.component';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 import { DashboardThemesTabComponent } from './dashboard/dashboard-themes-tab/dashboard-themes-tab.component';
 import { DashboardLearnProcessTabComponent } from './dashboard/dashboard-learn-process-tab/dashboard-learn-process-tab.component';
+import { ThemePageComponent } from './theme-page/theme-page.component';
 
 const routes: Routes = [
   {
@@ -22,7 +23,8 @@ const routes: Routes = [
             component: DashboardLearnProcessTabComponent
           }
         ]
-      }
+      },
+      {path: 'theme/:id', component: ThemePageComponent}
     ]
   }
 ]
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -11,6 +11,8 @@ import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-pag
 import { DashboardLearnProcessTabComponent } from './dashboard/dashboard-learn-process-tab/dashboard-learn-process-tab.component';
 import { DashboardThemesTabComponent } from './dashboard/dashboard-themes-tab/dashboard-themes-tab.component';
 import { ThemeService } from './shared/services/theme.service';
+import { ThemePageComponent } from './theme-page/theme-page.component';
+import { QuestionCardComponent } from './question-card/question-card.component';
 
 @NgModule({
   imports: [
@@ -25,10 +27,15 @@ import { ThemeService } from './shared/services/theme.service';
     HeaderComponent,
     DashboardPageComponent,
     DashboardLearnProcessTabComponent,
-    DashboardThemesTabComponent
+    DashboardThemesTabComponent,
+    ThemePageComponent,
+    QuestionCardComponent
   ],
   providers: [
     ThemeService
+  ],
+  exports: [
+    QuestionCardComponent
   ]
 })
 
